e2aad84c9cfe395ce4d6c28d4633e189771d6540
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-01-09 15:16:20 +0000

    Update cbus3_event_producer.ino

--- Test Sketches/Cbus3_event_producer/cbus3_event_producer.ino
+++ Test Sketches/Cbus3_event_producer/cbus3_event_producer.ino
@@ -25,21 +25,21 @@ void loop() {
   unsigned char data1[] = {0x99, 0x00, 0x00, 0x00, 0x02};
   sendCANMessage(0b01110000010, data1, sizeof(data1));
 
-  // Wait for 2 seconds
+  // Wait for 10 seconds
   delay(10000);
 
   // Send second CAN message
   unsigned char data2[] = {0x98, 0x00, 0x00, 0x00, 0x02};
   sendCANMessage(0b01110000010, data2, sizeof(data2));
 
-  // Wait for 2seconds before repeating the loop
+  // Wait for 30 seconds
   delay(30000);
 
   // Send third CAN message
   unsigned char data3[] = {0x98, 0x00, 0x00, 0x00, 0x03};
   sendCANMessage(0b01110000010, data3, sizeof(data3));
 
-  // Wait for 2seconds before repeating the loop
+  // Wait for 10 seconds before repeating the loop
   delay(10000);
 }
 
