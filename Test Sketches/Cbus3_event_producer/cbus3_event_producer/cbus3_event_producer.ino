23e8ea17484d5d7d1ee51e61ee53f7888a209a7b
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-10-03 04:48:44 +0100

    Code changed due to library changes

--- Test Sketches/Cbus3_event_producer/cbus3_event_producer/cbus3_event_producer.ino
+++ Test Sketches/Cbus3_event_producer/cbus3_event_producer/cbus3_event_producer.ino
@@ -1,3 +1,14 @@
+/*
+This sketch was part of my beginners journey into CBUS. While it is not the best way of coding,
+as I am not a qualified programmer, just code for fun.
+
+More details can be found in the GitHub Wiki
+
+https://github.com/johnmholmes/Arduino-CBUS--Introduction-Series/wiki
+
+This allowed me to see that the two units would work and I was able to desplay this in JMRI.
+*/
+
 #include <mcp_can.h>
 #include <SPI.h>
 
@@ -11,9 +22,9 @@ void setup() {
   Serial.begin(115200);
 
   // Initialize MCP2515
-  if (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK) {
+  if (CAN.begin(CAN_125KBPS, 8000000) == CAN_OK) {  
     Serial.println("MCP2515 Initialized Successfully!");
-    CAN.setMode(MCP_NORMAL);
+    
   } else {
     // Handle initialization error
     while (1);
