f5a6f10ef77e7c07d78fdd1b24962fc81d5686c4
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-10-03 04:35:39 +0100

    Update Cbus_01.ino

--- Test Sketches/Cbus_01/Cbus_01.ino
+++ Test Sketches/Cbus_01/Cbus_01.ino
@@ -18,8 +18,8 @@ MCP_CAN CAN(SPI_CS_PIN);
 
 void setup() {
   // Initialize MCP2515 with 8 MHz clock frequency
-  if (CAN.begin(CAN_125KBPS, 8000000) == CAN_OK) {  // Replaced MCP_8MHZ with 8000000
-    // No need to explicitly set the operating mode to normal
+  if (CAN.begin(CAN_125KBPS, 8000000) == CAN_OK) {  
+    
   } else {
     // Handle initialization error
     while (1);
@@ -31,14 +31,14 @@ void loop() {
   unsigned char data1[] = {0x99, 0x00, 0x00, 0x00, 0x02};
   sendCANMessage(0b01110000010, data1, sizeof(data1));
 
-  // Wait for 2 seconds
+  // Wait for 1 seconds
   delay(1000);
 
   // Send second CAN message
   unsigned char data2[] = {0x98, 0x00, 0x00, 0x00, 0x02};
   sendCANMessage(0b01110000010, data2, sizeof(data2));
 
-  // Wait for 2seconds before repeating the loop
+  // Wait for 2 seconds before repeating the loop
   delay(2000);
 }
 
