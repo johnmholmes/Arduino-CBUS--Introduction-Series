ec5b515589972af781ef2fda6a434e1b8ce32935
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-10-03 04:44:46 +0100

    Update Cbus_led1_v1.ino

--- Test Sketches/Cbus_led1_v1/Cbus_led1_v1.ino
+++ Test Sketches/Cbus_led1_v1/Cbus_led1_v1.ino
@@ -1,3 +1,14 @@
+/*
+This sketch was part of my beginners journey into CBUS. While it is not the best way of coding,
+as I am not a qualified programmer, just code for fun.
+
+More details can be found in the GitHub Wiki
+
+https://github.com/johnmholmes/Arduino-CBUS--Introduction-Series/wiki
+
+This allowed me to see that the two units would work and I was able to desplay this in JMRI.
+*/
+
 #include <mcp_can.h>
 #include <SPI.h>
 
@@ -11,10 +22,8 @@ void setup() {
   pinMode(CAN_INT_PIN, INPUT);
   Serial.begin(115200);
 
-  // Initialize MCP2515
-  if (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK) {
-    CAN.setMode(MCP_NORMAL);
-
+  // Initialize MCP2515 with 8 MHz clock frequency
+  if (CAN.begin(CAN_125KBPS, 8000000) == CAN_OK) {  
     // Attach interrupt for receiving messages
     attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), handleInterrupt, FALLING);
   } else {
@@ -33,8 +42,9 @@ void handleInterrupt() {
   byte len;
   byte buf[8];
 
-  // Read the CAN message
-  if (CAN.readMsgBuf(&id, &len, buf) == CAN_OK) {
+  // Read the CAN message (no parameters)
+  if (CAN.readMsgBuf(&len, buf) == CAN_OK) {  // Read message
+    id = CAN.getCanId();  // Retrieve the message ID
     Serial.print("Received message. ID: 0x");
     Serial.print(id, HEX);
     Serial.print("  Data: ");
@@ -60,18 +70,12 @@ void handleInterrupt() {
       digitalWrite(LED_PIN, HIGH);  // Turn on LED
       Serial.println("LED ON");
       
-} else if (len == 5 && buf[0] == 0x99 && buf[1] == 0x00 && buf[2] == 0x00 && buf[3] == 0x00 && buf[4] == 0x02) {
+    } else if (len == 5 && buf[0] == 0x99 && buf[1] == 0x00 && buf[2] == 0x00 && buf[3] == 0x00 && buf[4] == 0x02) {
       digitalWrite(LED_PIN, LOW);   // Turn off LED
       Serial.println("LED OFF");
-      
-}
+    }
 
   } else {
     Serial.println("Error reading CAN message");
   }
 }
-
-
-
-
-
